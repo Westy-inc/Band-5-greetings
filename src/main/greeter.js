a55e4c00aa9f6cdb2a9ae776cec481fa9c56ca9c
Westy-inc <65766828+Westy-inc@users.noreply.github.com>
2025-01-19 16:43:57 +0000

    capitalizer function added

--- src/main/greeter.js
+++ src/main/greeter.js
@@ -4,6 +4,11 @@ class Greeter {
     trimmer(name){
         return name.trim()
     }
+
+    capitalizer(name){
+        return name[0].toUpperCase() + name.substring(1)
+    }
+    
 }
 
 module.exports = Greeter
