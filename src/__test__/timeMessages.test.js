e677c5e58f11f75fb4e3f67a0ce9a549370c2186
Westy-inc <65766828+Westy-inc@users.noreply.github.com>
2025-01-19 17:23:50 +0000

    add lower bound tests

--- src/__test__/timeMessages.test.js
+++ src/__test__/timeMessages.test.js
@@ -3,30 +3,73 @@ const Greeter = require('../main/greeter');
 
 const greeter = new Greeter()
 
+// mocks time for this test suite
 
+// morning tets
 test('should return Good morning when the time is 06:00:00', ()=>{
-    jest.spyOn(global ,'Date').mockImplementation(() => ({ // mocks time for this test
+    jest.spyOn(global ,'Date').mockImplementation(() => ({ 
         toLocaleTimeString: () => "06:00:00",
     }));
     expect(greeter.getMessages()).toBe('Good morning')
 })
-test('should return Good afternoon when the time is 12:00:00', ()=>{
-    jest.spyOn(global ,'Date').mockImplementation(() => ({ // mocks time for this test
-        toLocaleTimeString: () => "12:00:00",
+
+
+test('should return Good afternoon when the time is 11:59:59', ()=>{
+    jest.spyOn(global ,'Date').mockImplementation(() => ({ 
+        toLocaleTimeString: () => "11:59:59",
     }));
-    expect(greeter.getMessages()).toBe('Good Afternoon')
+    expect(greeter.getMessages()).toBe('Good morning')
 })
 
+//afternoon tests
+test('should return Good afternoon when the time is 12:00:00', ()=>{
+    jest.spyOn(global ,'Date').mockImplementation(() => ({ 
+        toLocaleTimeString: () => "12:00:00",
+    }));
+    expect(greeter.getMessages()).toBe('Good afternoon')
+})
+
+test('should return Good afternoon when the time is 17:59:59', ()=>{
+    jest.spyOn(global ,'Date').mockImplementation(() => ({ 
+        toLocaleTimeString: () => "17:59:59",
+    }));
+    expect(greeter.getMessages()).toBe('Good afternoon')
+})
+
+
+//evening tests
 test('should return Good evening when the time is 18:00:00', ()=>{
-    jest.spyOn(global ,'Date').mockImplementation(() => ({ // mocks time for this test
+    jest.spyOn(global ,'Date').mockImplementation(() => ({
         toLocaleTimeString: () => "18:00:00",
     }));
-    expect(greeter.getMessages()).toBe('Good Afternoon')
+    expect(greeter.getMessages()).toBe('Good evening')
 })
+
+test('should return Good evening when the time is 21:59:59', ()=>{
+    jest.spyOn(global ,'Date').mockImplementation(() => ({
+        toLocaleTimeString: () => "21:59:59",
+    }));
+    expect(greeter.getMessages()).toBe('Good evening')
+})
+
+
+
+
+
+// night tests
+test('should return Good night when the time is 05:59:59', ()=>{
+    jest.spyOn(global ,'Date').mockImplementation(() => ({
+        toLocaleTimeString: () => "05:59:00",
+    }));
+    expect(greeter.getMessages()).toBe('Good night')
+})
+
+
 
 test('should return Good night when the time is 22:00:00', ()=>{
     jest.spyOn(global ,'Date').mockImplementation(() => ({ // mocks time for this test
-        toLocaleTimeString: () => "22:00:00",
+        toLocaleTimeString: () => " 22:00:00",
     }));
-    expect(greeter.getMessages()).toBe('Good Afternoon')
+    expect(greeter.getMessages()).toBe('Good night')
 })
+
