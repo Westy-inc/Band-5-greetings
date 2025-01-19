99ca120eb4f4f73b8482f4aa0f5053b1e7097507
Westy-inc <65766828+Westy-inc@users.noreply.github.com>
2025-01-19 16:41:44 +0000

    capitalizer test made

--- src/__test__/greeter.test.js
+++ src/__test__/greeter.test.js
@@ -1,9 +1,13 @@
+/* eslint-disable no-undef */
 const Greeter = require('../main/greeter');
 
 const greeter = new Greeter()
 
-// eslint-disable-next-line no-undef
+
 test('should trim name', ()=>{
-    // eslint-disable-next-line no-undef
     expect(greeter.trimmer(' john ')).toBe('john')
+})
+
+test('should capitalizes name' , ()=>{
+    expect(greeter.capitalizer('john')).toBe('John')
 })
